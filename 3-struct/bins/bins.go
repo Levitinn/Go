c5aa56405966eb13367cc2ff2a9231f9c1ce3f4d
Levitinn <44999446+Levitinn@users.noreply.github.com>
2026-06-24 18:34:18 +0300

    write save

--- 3-struct/bins/bins.go
+++ 3-struct/bins/bins.go
@@ -8,13 +8,15 @@ import (
 
 type BinList []Bin
 
+// Структура для хранения информации о бине
 type Bin struct {
-	ID        string
-	Name      string
-	CreatedAt time.Time
-	Private   bool
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"createdAt"`
+	Private   bool      `json:"private"`
 }
 
+// Функция для создания нового бина
 func NewBin(name string, private bool) *Bin {
 	return &Bin{
 		ID:        uuid.New().String(),
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,1 +1,35 @@
 package storage
+
+import (
+	"3-struct/bins"
+	"3-struct/file"
+	"encoding/json"
+	"os"
+)
+
+const FileName = "storage.json"
+
+func SaveToStorage(bin *bins.Bin) error {
+	list := bins.BinList{}
+	data, err := file.ReadFile(FileName)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+	} else {
+		err = json.Unmarshal(data, &list)
+		if err != nil {
+			return err
+		}
+	}
+	list = append(list, *bin)
+	bytes, err := json.Marshal(list)
+	if err != nil {
+		return err
+	}
+	err = file.WriteFile(bytes, FileName)
+	if err != nil {
+		return err
+	}
+	return nil
+}
