13fdddf9fa20e81df84e75034137c3fd38d12c82
Levitinn <44999446+Levitinn@users.noreply.github.com>
2026-07-13 16:16:37 +0300

    random-api

---
+++ 2-random-api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+func writeRandomNumber(w http.ResponseWriter, r *http.Request) {
+	randomNumber := rand.Intn(6)
+	w.Write([]byte(fmt.Sprintf("%d", randomNumber)))
+	return
+}
+func main() {
+	http.HandleFunc("/random", writeRandomNumber)
+	fmt.Println("Server is running on port 8081")
+	http.ListenAndServe(":8081", nil)
+}
